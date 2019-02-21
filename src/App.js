995b8aed6f0fe44133f4bd11775788e22c6dae9f
sylwuskak <sylwuskak@gmail.com>
2019-02-21 09:57:18 +0100

    burger builder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuider from './containers/BurgerBuilder/BurgerBulder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuider />
+        </Layout>
       </div>
     );
   }
